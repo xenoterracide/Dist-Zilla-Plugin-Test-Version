3687dbbda72ab731b75c960567326bac6ea073a0
Caleb Cushing <xenoterracide@gmail.com>
2011-05-28 13:31:45 -0500

    Build results of e77dff0 (on master)

--- lib/Dist/Zilla/Plugin/VersionTests.pm
+++ lib/Dist/Zilla/Plugin/VersionTests.pm
@@ -3,9 +3,35 @@ use 5.006;
 use strict;
 use warnings;
 BEGIN {
-	# VERSION
+	our $VERSION = '0.1.0'; # VERSION
 }
 
 1;
 
 # ABSTRACT: Dist::Zilla::Plugin::VersionTests
+
+__END__
+=pod
+
+=head1 NAME
+
+Dist::Zilla::Plugin::VersionTests - Dist::Zilla::Plugin::VersionTests
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 AUTHOR
+
+Caleb Cushing <xenoterracide@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2011 by Caleb Cushing.
+
+This is free software, licensed under:
+
+  The Artistic License 2.0 (GPL Compatible)
+
+=cut
+
