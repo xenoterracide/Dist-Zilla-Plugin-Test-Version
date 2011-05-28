4e1f2d27aca4ce443bef85d52330b4b6ac4ec39f
Caleb Cushing <xenoterracide@gmail.com>
2011-05-28 13:55:21 -0500

    first version of module written
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Dist/Zilla/Plugin/VersionTests.pm
+++ lib/Dist/Zilla/Plugin/VersionTests.pm
@@ -5,7 +5,23 @@ use warnings;
 BEGIN {
 	# VERSION
 }
+use Moose;
+extends 'Dist::Zilla::Plugin::InlineFiles';
 
+__PACKAGE__->meta->make_immutable;
 1;
 
-# ABSTRACT: Dist::Zilla::Plugin::VersionTests
+__DATA__
+__[ xt/release/test-version.t ]__
+#!/usr/bin/perl
+use 5.006;
+use strict;
+use warnings;
+
+eval "use Test::Version";
+plan skip_all => "Test::Version required for testing versions"
+	if $@;
+
+version_all_ok;
+
+# ABSTRACT: release Test::Version tests
