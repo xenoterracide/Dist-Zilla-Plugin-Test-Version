7617d2febd198d8728c26e21cdf4ba757a91ee0d
Karen Etheridge <ether@cpan.org>
2011-12-09 17:22:10 -0800

    use Test::Requires to test for minimum version, rather than a bare eval.
    Conflicts:
    
    	Changes
    	Makefile.PL
    	lib/Dist/Zilla/Plugin/Test/Version.pm
    
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/Dist/Zilla/Plugin/Test/Version.pm
+++ lib/Dist/Zilla/Plugin/Test/Version.pm
@@ -33,11 +33,9 @@ use strict;
 use warnings;
 use Test::More;
 
-BEGIN {
-    eval "use Test::Version 0.04";
-    plan skip_all => "Test::Version 0.04 or later required for testing versions"
-        if $@;
-}
+use Test::Requires {
+    'Test::Version' => 0.04,
+};
 
 version_all_ok;
 done_testing;
